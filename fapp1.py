5369185f7c425deadf8b572080402831586ef0ac
ivan555-lab <vnchapay@gmail.com>
2022-07-08 11:02:17 +0300

    ch4

--- fapp1.py
+++ fapp1.py
@@ -1,10 +1,22 @@
 #it is place for dev
-# changes in db 4
+
 
 
 import sqlite3
 
 with sqlite3.connect("fapp1.db") as db:
     cursor = db.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS comp(
+  id integer PRIMARY KEY,
+  Name text NOT NULL,
+  Category text NOT NULL,
+  Units text NOT NULL,
+  Price of pack text NOT NULL,
+  Package, gr. integer,
+  Cost of 1 gram, decimal);""")
+  
+
+
 
     
