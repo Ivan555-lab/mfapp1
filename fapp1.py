7cf034ef7f8a947ed7e6b87c689ab2892a0942c2
ivan555-lab <vnchapay@gmail.com>
2022-07-10 18:18:48 +0300

    db11

--- fapp1.py
+++ fapp1.py
@@ -1,11 +1,3 @@
-#it is place for dev
-#f28_1  next1
-#f28_2 start of dev
-# next step of dev (step 3)
-# step 4
-# step5
-# step6
-# db10
 
 import sqlite3
 
@@ -21,12 +13,11 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS comp(
   Package integer,
   Cost decimal);""")
 
-cursor.execute("""INSERT INTO comp(id, Name, Category, Units, Price, Package, Cost)
-VALUES("1", "Яйца", "яйца", "piece", "28.50", "1", "2.85")""")
-db.commit()
-db.close()
+
+
   
 
-
-
-    
+cursor.execute("""INSERT INTO comp(id, Name, Category, Units, Price, Package, Cost)
+VALUES("2", "Горох колотый", "Бакалея", "гр.", "35.99", "1000", "3.59")""")
+db.commit()
+db.close()   
