929cc3a2b16abd01e8dfbc9065849745e0685843
ivan555-lab <vnchapay@gmail.com>
2022-07-11 10:55:50 +0300

    2dbcf

--- fapp2.py
+++ fapp2.py
@@ -2,9 +2,13 @@ import sqlite3
 from tkinter import *
 
 def addtolist():
+    newid = sid.get()
     newname = sname.get()
     newcategory = scategory.get()
     newunits = sunits.get()
     newprice = sprice.get()
     newpackage = spackage.get()
     newcost = scost.get()
+    cursor.execute("""INSERT INTO comp (id, Name, Category, Units, Price, Package, Cost)
+VALUES(?, ?, ?, ?, ?, ?, ?)""",(newid, newname, newcategory, newunit, newprice, newunit, newcost))
+
