db518040a4894cb2ad030d3e5a5d56c4d6403d36
ivan555-lab <vnchapay@gmail.com>
2022-07-11 07:59:16 +0300

    5 rows

--- fapp1.py
+++ fapp1.py
@@ -4,23 +4,13 @@ import sqlite3
 with sqlite3.connect("fapp1.db") as db:
     cursor = db.cursor()
 
-cursor.execute("""CREATE TABLE IF NOT EXISTS comp(
-  id integer PRIMARY KEY,
-  Name text NOT NULL,
-  Category text NOT NULL,
-  Units text NOT NULL,
-  Price decimal NOT NULL,
-  Package integer,
-  Cost decimal);""")
-
-
-
-  
-
-
 
 
 cursor.execute("""INSERT INTO comp(id, Name, Category, Units, Price, Package, Cost)
-VALUES("3", "Вермешель", "Бакалея", "гр.", "24.96", "450", "5,54")""")
+VALUES("4", "Курятина", "Мясные", "гр.", "149.9", "1000", "14.99")""")
+db.commit()
+
+cursor.execute("""INSERT INTO comp(id, Name, Category, Units, Price, Package, Cost)
+VALUES("5", "Кефир", "Молочные", "гр.", "30.9", "900", "3.43")""")
 db.commit()
 db.close()
