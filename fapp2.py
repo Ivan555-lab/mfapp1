dddf42010ac74342eeff8e0fbc20645bc543310f
ivan555-lab <vnchapay@gmail.com>
2022-07-11 16:46:40 +0300

    4gui

--- fapp2.py
+++ fapp2.py
@@ -7,14 +7,23 @@ window.geometry("450x400")
 label1 = Label(text = "Enter component id: ")
 label1.place(x = 30, y = 35)
 sid = Entry(text="")
-sid.place(x = 150, y = 35, width = 200, height = 25)
+sid.place(x = 180, y = 35, width = 200, height = 25)
 sid.focus()
 
 label2 = Label(text = "Enter component name: ")
 label2.place(x = 30, y = 80)
 sname = Entry(text="")
-sname.place(x = 150, y = 80, width = 200, height = 25)
+sname.place(x = 180, y = 80, width = 200, height = 25)
 sname.focus()
+
+label3 = Label(text = "Enter component category: ")
+label3.place(x = 30, y = 125)
+scategory = Entry(text = "")
+scategory.place(x = 180, y = 125, width = 200, height = 25)
+scategory.focus()
+
+
+
 
 window.mainloop()
 db.close()
