84e378a52a7db47a5b489de91f6c720d6680e131
ivan555-lab <vnchapay@gmail.com>
2022-07-11 16:21:08 +0300

    3gui

--- fapp2.py
+++ fapp2.py
@@ -1,9 +1,24 @@
+from tkinter import *
+
 window = Tk()
 window.title("Comp")
-window.geometry("450x400)
+window.geometry("450x400")
 
-label1 = Label(text = "Enter comp id: ")
+label1 = Label(text = "Enter component id: ")
 label1.place(x = 30, y = 35)
+sid = Entry(text="")
+sid.place(x = 150, y = 35, width = 200, height = 25)
+sid.focus()
+
+label2 = Label(text = "Enter component name: ")
+label2.place(x = 30, y = 80)
+sname = Entry(text="")
+sname.place(x = 150, y = 80, width = 200, height = 25)
+sname.focus()
+
+window.mainloop()
+db.close()
+
                 
                 
 
