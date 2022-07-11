e8f6d937493bdd45b756da3cbc57f50fd19b8c22
ivan555-lab <vnchapay@gmail.com>
2022-07-11 13:47:35 +0300

    4dbcf

--- fapp2.py
+++ fapp2.py
@@ -10,8 +10,18 @@ def addtolist():
     newpackage = spackage.get()
     newcost = scost.get()
     cursor.execute("""INSERT INTO comp (id, Name, Category, Units, Price, Package, Cost)
-VALUES(?, ?, ?, ?, ?, ?, ?)""",(newid, newname, newcategory, newunit, newprice, newunit, newcost))
+VALUES(?, ?, ?, ?, ?, ?, ?)""",(newid, newname, newcategory, newunits, newprice, newunit, newcost))
     db.commit()
     sid.delete(0, END)
+    sname.delete(0, END)
+    scategory.delete(0, END)
+    sunits.delete(0, END)
+    sprice.delete(0, END)
+    spackage.delete(0, END)
+    scost.delete(0, END)
+    sname.focus()
+    
+    
+    
     
     
