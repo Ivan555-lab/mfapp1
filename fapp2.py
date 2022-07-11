7d7618a4c6c9ce12f8435e552e0ff879b91ee95d
ivan555-lab <vnchapay@gmail.com>
2022-07-11 14:24:52 +0300

    2gui

--- fapp2.py
+++ fapp2.py
@@ -2,6 +2,9 @@ window = Tk()
 window.title("Comp")
 window.geometry("450x400)
 
+label1 = Label(text = "Enter comp id: ")
+label1.place(x = 30, y = 35)
+                
                 
 
                 
