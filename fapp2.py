e46ba032a04f190513a076379983664d9dc71422
ivan555-lab <vnchapay@gmail.com>
2022-07-11 10:42:08 +0300

    1dbcf

---
+++ fapp2.py
@@ -0,0 +1,10 @@
+import sqlite3
+from tkinter import *
+
+def addtolist():
+    newname = sname.get()
+    newcategory = scategory.get()
+    newunits = sunits.get()
+    newprice = sprice.get()
+    newpackage = spackage.get()
+    newcost = scost.get()
